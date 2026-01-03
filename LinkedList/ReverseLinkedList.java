60120a1928fd206e73da9da906f8e40c0e29c7de
Paridhi <paridhibabel239@gmail.com>
2026-01-04 01:00:01 +0530

    Add linked list and factorial recursion solutions

---
+++ LinkedList/ReverseLinkedList.java
@@ -0,0 +1,26 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+public class ReverseLinkedList {
+
+    public static ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+
+        while (current != null) {
+            ListNode nextNode = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextNode;
+        }
+        return prev;
+    }
+
+}
